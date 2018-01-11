ff3b1f3e6d4f14b9da50d7cdae857717df066da4
Jason Rowland <jasonrowland86>
2018-01-11 10:38:43 -0500

    server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  console.log(res);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.status(400).json({
+    message: 'Endpoint not found'
+  });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port);
+
+console.log(`Listening on ${port}`);
